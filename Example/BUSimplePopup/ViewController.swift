cdc0e3a0f9e99b5f1e3e35b241569cdabca94c07
Burak Üstün <burakustn@gmail.com>
2017-06-09 14:00:18 +0300

    Initial Commit

--- Example/BUSimplePopup/ViewController.swift
+++ Example/BUSimplePopup/ViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import BUSimplePopup
 
 class ViewController: UIViewController {
 
+    let PopupManager = BUSimplePopupManager()
     override func viewDidLoad() {
         super.viewDidLoad()
+        PopupManager.PopupsBackgroundColor = .white
+        PopupManager.PopupsTitleTextColor  = .black
+        PopupManager.PopupsBodyTextColor   = .darkGray
+        PopupManager.PopupBackgroundAlpha  = 1.0
+        PopupManager.PopupsTitleFont       = UIFont.init(name: "Futura", size: 20)!
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,5 +28,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnPop_Click(_ sender: Any) {
+         PopupManager.showPopup(Popup: BUSimplePopup.init(_title: "BUSimplePopup", _body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", _image: UIImage.init(named: "logo")))
+    }
 }
 
